f7ea482bff28d83b1c18e621a337b3f5ce84cb7b
qianlaoshi <yidaios5@126.com>
2021-08-16 21:45:09 +0800

    提交新工程

---
+++ src/main/java/com/yida/gitdemo/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.yida.gitdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
