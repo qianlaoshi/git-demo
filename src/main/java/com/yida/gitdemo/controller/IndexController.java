2585cec2e2c59b9c1dc91371b6143ce70464d10a
qianlaoshi <yidaios5@126.com>
2021-08-16 22:09:00 +0800

    修改了文件

--- src/main/java/com/yida/gitdemo/controller/IndexController.java
+++ src/main/java/com/yida/gitdemo/controller/IndexController.java
@@ -9,4 +9,9 @@ public class IndexController {
     public String hello() {
         return "hello world";
     }
+
+    @RequestMapping("/world")
+    public String world() {
+        return "hello 宜达互联";
+    }
 }
